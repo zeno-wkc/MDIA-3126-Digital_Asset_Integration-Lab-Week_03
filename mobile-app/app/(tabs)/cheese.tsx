12a3c1baf2c07f15fda63dec4f96df4e614e9930
Zeno <zenowkc@gmail.com>
2024-10-02 23:07:47 -0700

    Update lesson 04

--- mobile-app/app/(tabs)/cheese.tsx
+++ mobile-app/app/(tabs)/cheese.tsx
@@ -7,8 +7,11 @@ import { Link } from 'expo-router';
 
 export default function Page() {
   return (
-    <View>
-      <Text>This is my "home" page</Text>
+    <View className="bg-red-600">
+      <Text className="text-lg border-2 border-violet-800">This is my "home" page</Text>
+      <Text className="text-lg border-2 border-green-900">This is my "home" page</Text>
+      <Text className="text-lg border-2 border-blue-700">This is my "home" page</Text>
+      <Text className="text-lg border-4 border-sky-800 bg-green-600">This is my "home" page</Text>
       <Link href="/sandwich">sandwich</Link>
       <Link href="/cheese">cheese</Link>
     </View>
